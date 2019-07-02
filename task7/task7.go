ca90fbb12e283d39f136ba426b7961e0d5d9aa42
Bogdan <prepelitsa.b@gmail.com>
2019-07-02 16:54:47 +0300

    some refactoring, remake func makeSliceResult

--- task7/task7.go
+++ task7/task7.go
@@ -6,38 +6,45 @@ import (
 	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
-func outPut(n int) {
+func MakeSliceResult(n uint64)  []string{
+	var answer []string
 	if n == 1 {
-		fmt.Println("")
-		return
+		answer=make([]string,1)
+		answer[0] = ""
+		return answer
 	}
-	fmt.Printf("1")
-	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
-		fmt.Printf(", %d", i)
+	countOfElements:= int(math.Ceil(math.Sqrt(float64(n)))-1) //get count of elements (round to the least integer value greater than or equal sqrt(n) and -1)
+	fmt.Println(countOfElements)
+	answer=make([]string,countOfElements)
+	for i := 1; i <= countOfElements; i++ {
+		answer[i-1] = strconv.FormatInt(int64(i), 10)
 	}
-	fmt.Printf("\n")
+	return answer
 }
-func argsCheck(args []string) (int, error) {
+func ArgsCheck(args []string) (uint64, error) {
 	if len(args) == 0 {
 		return 0, errors.New("HELP: This program helps to show natural numbers that have square less than inputed n. If you want to start write integer n!")
 	}
 	if len(args) > 1 {
 		return 0, errors.New("there should be only one param n!")
 	}
-	n, err := strconv.Atoi(args[0])
+	n, err := strconv.ParseUint(args[0], 10, 64) 
 	if err != nil {
-		return 0, errors.New("something wrong with your n! It must be digit!")
+		return 0, fmt.Errorf("something wrong with your n: %s", err)
 	}
 	return n, nil
 }
 func main() {
 	args := os.Args[1:]
-	n, err := argsCheck(args)
+	n, err := ArgsCheck(args)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	outPut(n)
+	answ:= MakeSliceResult(n)
+	outPut:=strings.Join(answ,",")
+	fmt.Println(outPut)
 }
